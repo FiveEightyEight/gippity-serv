f945e92beea79810828ebb6f0f33d943b04623a2
FiveEightyEight <robert.abreu3@gmail.com>
2024-08-14 21:00:14 -0400

    add github.com/joho/godotenv mod

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"log"
+	"github.com/joho/godotenv"
 )
+
+func getEnvKey() string {
+	err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file")
+    }
+
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+        fmt.Println("API_KEY is not set")
+    } else {
+        fmt.Printf("The OpenAI API Key is: %s\n", apiKey)
+    }
+	return apiKey
+}
 
 func main() {
 	// come back later
-	fmt.Println("hey this is in progress...")
+	key := getEnvKey()
+	fmt.Println("hey this is in progress...", key)
 }
