43e633c04e5d71108c5f646f887f7563313bb5e3
FiveEightyEight <robert.abreu3@gmail.com>
2024-08-15 23:08:11 -0400

    add structs for api calls to openai

--- cmd/main.go
+++ cmd/main.go
@@ -2,30 +2,41 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"log"
+	"os"
+
 	"github.com/joho/godotenv"
 )
 
-models := map[string]string{
-	"GPT-4o" : "gpt-4o",
-	"GPT-4o mini": "gpt-4o-mini",
-	"GPT-4 Turbo": "gpt-4-turbo",
+var models = map[string]string{
+	"GPT-4o":        "gpt-4o",
+	"GPT-4o mini":   "gpt-4o-mini",
+	"GPT-4 Turbo":   "gpt-4-turbo",
 	"GPT-3.5 Turbo": "gpt-3.5-turbo-0125",
+}
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type ChatBody struct {
+	Model    string    `json:"model`
+	Messages []Message `json:"messages"`
 }
 
 func getEnvKey() string {
 	err := godotenv.Load()
-    if err != nil {
-        log.Fatalf("Error loading .env file")
-    }
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 
 	apiKey := os.Getenv("API_KEY")
 	if apiKey == "" {
-        fmt.Println("API_KEY is not set")
-    } else {
-        fmt.Printf("The OpenAI API Key is: %s\n", apiKey)
-    }
+		fmt.Println("API_KEY is not set")
+	} else {
+		fmt.Printf("The OpenAI API Key is: %s\n", apiKey)
+	}
 	return apiKey
 }
 
