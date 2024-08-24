c048e53adf2ee49a209deeefcd398b154bc0d5d8
FiveEightyEight <robert.abreu3@gmail.com>
2024-08-23 22:08:28 -0400

    load db url from env properly

--- db/database.go
+++ db/database.go
@@ -2,16 +2,24 @@ package db
 
 import (
 	"context"
+	"log"
+	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 )
 
 type Database struct {
 	Pool *pgxpool.Pool
 }
 
-func NewDatabase(connString string) (*Database, error) {
-	pool, err := pgxpool.Connect(context.Background(), connString)
+func NewDatabaseConnection() (*Database, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	connString := os.Getenv("DATABASE_URL")
+	pool, err := pgxpool.New(context.Background(), connString)
 	if err != nil {
 		return nil, err
 	}
